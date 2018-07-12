327a3dfa588b394542c17cdd6bb9e582bbc09e9c
Raghav Ramani Raju Thirumulu <rrthirumulu@gmail.com>
2018-07-12 15:42:07 -0700

    Added code for calculating weights for barycentric formula

---
+++ hw2/barycentric_weights.m
@@ -0,0 +1,29 @@
+function T = barycentric_weights(x)
+% Computer code for finding the weights of the Barycentric Formula
+% Input:  x --- input points;
+% Output: T --- vector of weights to plug into barycentric.m
+%
+% Author: Raghav Thirumulu, Perm 3499720
+% Date:   07/11/2018
+
+% Find length of our input list, create another vector to substitute 
+% weights
+n=length(x);
+T=zeros(1,n);
+
+% Iterate through the input list and with interpolation, use the formula 
+% for calculating the weights
+for j=1:n
+    w=1;
+    for k=1:n
+        if j~=k
+            w = w*(x(j)-x(k));
+        end
+    end
+    T(j)=w;
+end
+
+% We want the reciprocal of the calculation of w
+T=1./T;
+
+end
