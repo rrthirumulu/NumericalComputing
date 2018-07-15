769ba0a3b55b02a689ae95fabeafad2ff8ea316e
Raghav Ramani Raju Thirumulu <rrthirumulu@gmail.com>
2018-07-14 18:34:28 -0700

    Created file/function for evaluating Newton polynomial for question 3

---
+++ hw3/newton.m
@@ -0,0 +1,12 @@
+function F = newton(x,y,xbar,n)
+% Computer code for evaluating the Newton polynomial based on a set of
+% given points and degree
+% Input:  x    --- Sample x points for evaluating polynomial
+%         y    --- Sample y points for evaluating polynomial
+%         xbar --- Point we want to evaluate at
+%         n    --- degree of Newton polynomial
+%
+% Author: Raghav Thirumulu, Perm 3499720
+% Date:   07/10/2018
+
+end
