91c960d2b19fecc834c8fc791934dc091712a23a
Raghav Ramani Raju Thirumulu <rrthirumulu@gmail.com>
2018-07-14 20:05:30 -0700

    Finished working version of Newton's divided difference

--- hw3/newton.m
+++ hw3/newton.m
@@ -1,4 +1,4 @@
-function F = newton(x,y,xbar,n)
+function T = newton(x,y,xbar,n)
 % Computer code for evaluating the Newton polynomial based on a set of
 % given points and degree
 % Input:  x    --- Sample x points for evaluating polynomial
@@ -8,5 +8,23 @@ function F = newton(x,y,xbar,n)
 %
 % Author: Raghav Thirumulu, Perm 3499720
 % Date:   07/10/2018
+
+% Create array of n+1 entries for computing values of c
+c=zeros(1,n+1);
+T=0;
+
+% Iterate through, computing divided difference for each value
+% We use n+1 because matrix values begin with 1 in MATLAB
+for i=1:(n+1)
+    f = 1;
+    for j=1:(n+1)
+        % We do not want a denominator of 0
+        if j~=i
+            f = f * (xbar-x(j))/(x(i)-x(j));
+        end
+    end
+    % Sum each term to find the approximation
+    T = T + y(i)*f;
+end
 
 end
